7901e4f1b559aeaa3a395a01b4289208b68a9378
ko0hi <ko0hi.4731@gmail.com>
2023-05-14 19:44:40 +0900

    editOrderの追加

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import positionsRouter from './positions'
 import createOrderRouter from './createOrder'
 import cancelOrderRouter from './cancelOrder'
 import ordersRouter from './orders'
+import editOrderRouter from './editOrder'
 import implicitApiRouter from './implicits'
 import passExchangeIdMiddleware from '../middlewares/exchangeId'
 
@@ -13,6 +14,7 @@ const router = Router()
 router.use('/:exchangeId/v1/markets', passExchangeIdMiddleware, marketsRouter)
 router.use('/:exchangeId/v1/createOrder', passExchangeIdMiddleware, createOrderRouter)
 router.use('/:exchangeId/v1/cancelOrder', passExchangeIdMiddleware, cancelOrderRouter)
+router.use('/:exchangeId/v1/editOrder', passExchangeIdMiddleware, editOrderRouter)
 router.use('/:exchangeId/v1/myTrades', passExchangeIdMiddleware, myTradesRouter)
 router.use('/:exchangeId/v1/positions', passExchangeIdMiddleware, positionsRouter)
 router.use('/:exchangeId/v1/orders', passExchangeIdMiddleware, ordersRouter)
